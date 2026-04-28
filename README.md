57bc7fb1a87a2eb6a89242a8f6ddb33c34ebf3bd
Sujata1694 <singh.sujata1694@gmail.com>
2026-04-28 17:14:24 +0530

    Initial Commit Ecommerce FullStack Project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ecommerce Full Stack Project
+
+Tech Stack:
+- React (Frontend)
+- Spring Boot (Backend)
+- MySQL Database
+
+## Run Frontend
+cd frontend
+npm install
+npm start
+
+## Run Backend
+cd backend
+mvn spring-boot:run
---
+++ ecommerce/src/main/java/com/example/ecommerce/cart/CartRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUserId(Long userId);
+}
